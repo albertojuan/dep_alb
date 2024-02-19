59001eb7bc73163da9979ed3040fec25080dd0b6
Alberto Juan <alberto@tinybird.co>
2024-02-19 18:56:36 +0100

    Remove VERSION in files

---
+++ deploy/0.0.1/cd-deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Skipping CD deploy as this is a file naming migration to deprecate VERSION by file feature. Dont forget to execute `tb init --override-commit...`"
---
+++ deploy/0.0.1/ci-deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Skipping CI deploy as this is a file naming migration to deprecate VERSION by file feature."
